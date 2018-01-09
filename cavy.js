06c8f71126ddf5c052af3c3ad918266e9943f9aa
David Somers <david@jalada.co.uk>
2018-01-09 14:54:25 +0000

    Tidy up command definition, add run-android

--- cavy.js
+++ cavy.js
@@ -1,11 +1,34 @@
-#!/usr/bin/env node
+#!/usr/bin/env node --harmony
 
 const program = require('commander');
-const figlet = require('figlet')
+const { spawn } = require('child_process');
 
-program
-  .version('0.0.1')
-  .command('run-ios', 'run `react-native run-ios` and capture Cavy test results')
-  .parse(process.argv)
+const server = require('./server')
 
-console.log(figlet.textSync('Cavy', {font: 'isometric3'}));
+let reactNativeCommand;
+
+program.
+  version('0.0.1').
+  option('-p, --port <port>', 'port to listen on', 8082, parseInt).
+  arguments('<run-ios|run-android>', 'react-native command to run').
+  action((cmd) => reactNativeCommand = cmd).
+  parse(process.argv);
+
+if (reactNativeCommand !== 'run-ios' && reactNativeCommand !== 'run-android') {
+  program.outputHelp();
+  process.exit(1);
+}
+
+console.log(`cavy: running \`react-native ${reactNativeCommand}\`...`);
+let rn = spawn('react-native', [reactNativeCommand], {stdio: 'inherit'});
+rn.on('close', (code) => {
+  console.log(`cavy: react-native exited with code ${code}.`);
+  if (code) {
+    return process.exit(code);
+  }
+
+  // Start test server, listening for test results to be posted
+  const app = server.listen(4000, () => {
+    console.log('cavy: running Cavy tests...');
+  });
+});
