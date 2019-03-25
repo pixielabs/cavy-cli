5181b95decb6a491654781a34e9c07cbc57cf37e
Abigail McPhillips <abigail.mcphillips@gmail.com>
2019-03-25 11:13:02 +0000

    Nicer error message with hint

--- src/init.js
+++ src/init.js
@@ -5,28 +5,33 @@ const indexContent = require('../templates/index.test.js');
 const specContent = require('../templates/exampleSpec.js');
 const { existsSync, writeFileSync, mkdirSync } = require('fs');
 
+DEFAULT_TEST_DIR = 'specs';
+
 // Sets up Cavy entry index file and example spec.
 function init(args) {
-  if (existsSync('index.js')) {
-    console.log('Adding Cavy to your project...');
+  console.log('cavy: Adding Cavy to your project...');
 
-    // The first additional argument to cavy init is taken to be the name
-    // of the test folder. If nothing is provided, then fall back on `specs`.
-    // This is used in the index.test.js file too.
-    const folderName = args[1] || 'specs';
+  // The first additional argument to cavy init is the name of the test
+  // directory. This is used in the index.test.js file too.
+  const folderName = args[1] || DEFAULT_TEST_DIR;
 
-    // Create a exampleTest.js file in the specified folder in the route of the
-    // project.
-    mkdirSync(`./${folderName}`);
-    writeFileSync(`./${folderName}/exampleSpec.js`, specContent);
-
-    // Create an index.test.js file in the route of the project.
-    writeFileSync('index.test.js', indexContent(folderName));
-
-    // Exit
-    console.log('Done!');
-    process.exit(1);
+  if (existsSync(folderName)) {
+    console.log(`cavy: Looks like a ./${folderName} directory already exists for this project.`);
+    console.log('cavy: To continue set up, re-run the command with an alternative test directory name: `cavy init <test-directory>`')
+    process.exit(0)
   }
+
+  // Create a exampleTest.js file in the specified folder in the route of the
+  // project.
+  mkdirSync(`./${folderName}`);
+  writeFileSync(`./${folderName}/exampleSpec.js`, specContent);
+
+  // Create an index.test.js file in the route of the project.
+  writeFileSync('index.test.js', indexContent(folderName));
+
+  // Exit
+  console.log('Done!');
+  process.exit(1);
 }
 
 module.exports = init;
